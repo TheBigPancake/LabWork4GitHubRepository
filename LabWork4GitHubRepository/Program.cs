953334fba0a85fca1cf9b3314c51ca494f13386f
BTGert <burlaemanuel12@gmail.com>
2021-04-12 11:34:32 +0300

    Базовая версия

--- LabWork4GitHubRepository/Program.cs
+++ LabWork4GitHubRepository/Program.cs
@@ -14,11 +14,9 @@ namespace LabWork4GitHubRepository
             do
             {
                 Console.Clear();
-                Console.WriteLine("Вариант 2\nвыбирите задание:");
-                Console.WriteLine("1) задача 1");
-                Console.WriteLine("2) задача 2");
-                Console.WriteLine("3) задача 3");
-                Console.Write("0) выход\n>");
+                Console.WriteLine("Работа с удаленными репозиториями\nВыбирете сообщение:");
+                Console.WriteLine("\t1) Сообщение Городенского Станислава");
+                Console.Write("\t0) выход\n>");
             } while (Switch(int.Parse(Console.ReadLine())));
         }
 
@@ -27,9 +25,6 @@ namespace LabWork4GitHubRepository
             Console.Clear();
             switch (key)
             {
-                case 1: new ProgramOne().PlayProgram(); break;
-                case 2: new ProgramTwo().PlayProgram(); break;
-                case 3: new ProgramThree().PlayProgram(); break;
                 case 0: return false;
             }
             return true;
---
+++ LabWork4GitHubRepository/RunebleClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWork4GitHubRepository
+{
+    interface RunebleClass
+    {
+        public void Run();
+    }
+}
