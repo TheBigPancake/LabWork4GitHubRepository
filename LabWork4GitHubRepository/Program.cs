c5146baf672c9dd7722d4181891c32587769ccf2
BTGert <burlaemanuel12@gmail.com>
2021-04-12 12:14:12 +0300

    Добавил свой класс

--- LabWork4GitHubRepository/Program.cs
+++ LabWork4GitHubRepository/Program.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using LabWork4GitHubRepository.TestClass;
+using System;
 
 namespace LabWork4GitHubRepository
 {
     class Program
     {
-        private static RunebleClass[] classes = { };
+        private static RunebleClass[] classes = { new ATestClass()};
         static void Main(string[] args)
         {
             Menu();
@@ -17,6 +18,7 @@ namespace LabWork4GitHubRepository
                 Console.Clear();
                 Console.WriteLine("Работа с удаленными репозиториями\nВыбирете сообщение:");
                 Console.WriteLine("\t1) Сообщение Городенского Станислава");
+                Console.WriteLine("\t2) Тестовое сообщение");
                 Console.Write("\t0) выход\n>");
             } while (ClearConsloeAndRunClass(int.Parse(Console.ReadLine())));
         }
