fde348c8d728cfb15bab7b32c70a34be635dc0f6
BTGert <burlaemanuel12@gmail.com>
2021-04-12 09:48:39 +0300

    Добавьте файлы проекта.

---
+++ LabWork4GitHubRepository/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabWork4GitHubRepository
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
