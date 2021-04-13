39a9c2374b1acfc5a3b21175ac47c39e64ed50c2
BTGert <burlaemanuel12@gmail.com>
2021-04-13 11:48:15 +0300

    Dead end

---
+++ LabWork4GitHubRepository/Gorodensky/GorodenskyAddedClass.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWork4GitHubRepository.Gorodensky
+{
+    class GorodenskyAddedClass : RunebleClass
+    {
+        public void Run()
+        {
+            Console.WriteLine("Шутка юмора».");
+            Console.ReadKey();
+        }
+    }
+}
